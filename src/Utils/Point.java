3834437bad95dbad69e345779d9d788b62236d21
RATCM <isakbjoershol@gmail.com>
2024-10-04 19:43:50 +0200

    Added implementation for Point class

--- src/Utils/Point.java
+++ src/Utils/Point.java
@@ -1,5 +1,19 @@
 package Utils;
 
 public class Point {
+    public int x;
+    public int y;
 
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Point Add(Point p1, Point p2){
+        return new Point(p1.x+p2.x, p1.y+p2.y);
+    }
+    
+    public static Point Subtract(Point p1, Point p2){
+        return new Point(p1.x-p2.x, p1.y-p2.y);
+    }
 }
