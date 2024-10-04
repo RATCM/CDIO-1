abafd3b5d5f95d2ec2a1ed8b37733e93b74e850d
RATCM <isakbjoershol@gmail.com>
2024-10-04 19:46:38 +0200

    Added implementation for Size class

--- src/Utils/Size.java
+++ src/Utils/Size.java
@@ -1,5 +1,19 @@
 package Utils;
 
 public class Size {
+    public int width;
+    public int height;
 
+    public Size(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public static Size Add(Size s1, Size s2){
+        return new Size(s1.width+s2.width, s1.height+s2.height);
+    }
+    
+    public static Size Subtract(Size s1, Size s2){
+        return new Size(s1.width-s2.width, s1.height-s2.height);
+    }
 }
