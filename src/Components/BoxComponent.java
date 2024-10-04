55a45eada5a20fa9d1fd5bdac5fdd1a4ae9b0951
RATCM <isakbjoershol@gmail.com>
2024-10-04 20:42:54 +0200

    Added BoxComponent class

---
+++ src/Components/BoxComponent.java
@@ -0,0 +1,26 @@
+package Components;
+
+import Utils.Console;
+import Utils.Point;
+import Utils.Size;
+
+public class BoxComponent extends Component {
+    private char _fillChar;
+
+    public BoxComponent(Point location, Size size, char fillChar) {
+        super(location, size);
+        this._fillChar = fillChar;
+    }
+
+    @Override
+    public void update() {
+        Point topLeft = this.location;
+        Point bottomRight = new Point(location.x + size.width, location.y + size.height);
+
+        for(Point point = topLeft; point.y < bottomRight.y; point.y++){
+            Console.setCursorPosition(point);
+            String outRow = String.valueOf(_fillChar).repeat(this.size.width);
+            System.out.println(outRow);
+        }
+    }
+}
