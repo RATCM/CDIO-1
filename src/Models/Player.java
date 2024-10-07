6eb0cad0cbf8067543997021fa3cc4775057f713
ratcm <isakbjoershol@mbptilhendeisak.students.clients.local>
2024-10-07 14:01:24 +0200

    Added player model

---
+++ src/Models/Player.java
@@ -0,0 +1,11 @@
+package Models;
+
+public class Player {
+    public final String name;
+    public int points;
+
+    public Player(String name){
+        this.name = name;
+        points = 0;
+    }
+}
