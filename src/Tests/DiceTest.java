763c6665e880b29de8a6a154290d8270a5be8015
ih8mondays <bjoerkobel@hotmail.dk>
2024-10-10 13:53:18 +0200

    fixed meanArray and s_v_Array

--- src/Tests/DiceTest.java
+++ src/Tests/DiceTest.java
@@ -3,22 +3,26 @@ import Models.DieModel;
 
 public class DiceTest {
 
+    
+
     public static String diceSixTest() {
 
         var Die = new DieModel();
         int rollCount = 1000;
+        double avgDis = rollCount/6;        
     
         int[] distribution = new int[6];
 
         for(int i = 0; i < rollCount ; i++){ //Rolls and stores in array
-        Die.roll();
-        var currentDieVal = Die.getValue();
+            Die.roll();
+            var currentDieValue = Die.getValue();
 
-        distribution[currentDieVal-1] ++ ;
+            distribution[currentDieValue-1] ++ ;
 
         }
-
-        var avgDis = rollCount/6;
+        
+        double mean = meanArray(distribution, rollCount);
+        double standardDeviation = s_v_Array(distribution, mean, rollCount);
 
         String testResult = "Test Result of Die \n" +
                             "Amount of 1's: " + distribution[0] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[0]-avgDis)*100.0/avgDis + "%" +
@@ -26,8 +30,26 @@ public class DiceTest {
                           "\nAmount of 3's: " + distribution[2] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[2]-avgDis)*100.0/avgDis + "%" +
                           "\nAmount of 4's: " + distribution[3] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[3]-avgDis)*100.0/avgDis + "%" +
                           "\nAmount of 5's: " + distribution[4] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[4]-avgDis)*100.0/avgDis + "%" +
-                          "\nAmount of 6's: " + distribution[5] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[5]-avgDis)*100.0/avgDis + "%" ;
+                          "\nAmount of 6's: " + distribution[5] + "  Expected amount: " + avgDis + "   Deviation: " + (distribution[5]-avgDis)*100.0/avgDis + "%" +
+                          "\nStandard Deviation: " + standardDeviation + "    " + mean ;
         return testResult;
     }
 
+    static double s_v_Array(int values[], double mean, int rollCount) {
+        double sumVar = 0;
+        for (int i = 0 ; i < values.length ; i++) {
+            sumVar += Math.pow( (i + 1)  - mean , 2)*values[i];
+        }
+        double s_v_ = Math.sqrt( sumVar / rollCount ) ;
+        return s_v_ ;
+    }
+
+    static double meanArray(int[] values, int rollCount) {
+        double sum = 0;
+        for (int i = 0 ; i < values.length ; i++) {
+            sum += values[i]*(i+1);
+        }
+        double mean = sum / rollCount ;
+        return mean;
+    }
 }
